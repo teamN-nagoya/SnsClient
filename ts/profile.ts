0e25adabbc360751a3617be55c43bcdb7afa382e
yuuki1101927 <yuuki_21n1101927@nnn.ed.jp>
2021-09-07 11:56:30 +0900

    profileのハッシュ部分を作成

--- ts/profile.ts
+++ ts/profile.ts
@@ -1,4 +1,13 @@
 import { digestMessage } from "./util"
 import { webSocket } from "./webSocket"
+import { MessagesRequestC2SPacket } from "./packets/c2s/MessagesRequestC2SPacket"
 
-console.log("aa")
+
+let userId = location.hash
+if(!userId) {
+    location.hash = "yuuki1101927"
+    userId = "yuuki1101927"
+}
+
+const request = new MessagesRequestC2SPacket(userId)
+console.log(request)
