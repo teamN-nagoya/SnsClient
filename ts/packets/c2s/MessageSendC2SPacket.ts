2ae9fae2a0911a7fbbb34a552fb4ec0457a2de6e
yuuki1101927 <yuuki_21n1101927@nnn.ed.jp>
2021-09-10 23:03:21 +0900

    メッセージをポストできるように

---
+++ ts/packets/c2s/MessageSendC2SPacket.ts
@@ -0,0 +1,13 @@
+import { C2SPacket } from "../C2SPacket";
+
+export class MessageSendC2SPacket extends C2SPacket {
+	readonly MessageSendC2SPacketType:null = null;
+	userId: string;
+	message: string;
+    
+	constructor(userId:string,message:string) {
+		super()
+	    this.userId = userId
+	    this.message = message
+	}
+}
--- ts/post.ts
+++ ts/post.ts
@@ -1,3 +1,15 @@
 import * as Common from "./common/left"
+import { webSocket } from "./common/webSocket"
+import { MessageSendC2SPacket } from "./packets/c2s/MessageSendC2SPacket"
 
 Common.init()
+
+const postElement = document.getElementById("postTextbox") as HTMLInputElement
+const submitElement = document.getElementById("postBtn") as HTMLInputElement
+
+submitElement.addEventListener("click",(event)=>{
+    if(Common.hash.myId !== undefined) {
+        const packet = new MessageSendC2SPacket(Common.hash.myId,postElement.value)
+        webSocket.send(JSON.stringify(packet))
+    }
+})
